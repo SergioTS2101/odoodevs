de5e55c1a49e3276b156879ef986692605275b01
mmartine <miguel.mo1999@gmail.com>
2022-02-16 12:47:47 +0100

    Actualizando módulo lista tareas

--- odoo14-custom-addons/lista_tareas/models/models.py
+++ odoo14-custom-addons/lista_tareas/models/models.py
@@ -11,21 +11,22 @@ class lista_tareas(models.Model):
     #Elementos de cada fila del modelo de datos
     #Los tipos de datos a usar en el ORM son 
     # https://www.odoo.com/documentation/14.0/developer/reference/addons/orm.html#fields
-   
+    avatar = fields.Image('Imagen tarea', max_width = 50, max_heigth = 50)
     tarea = fields.Char()
+    fecha = fields.Date()
     prioridad = fields.Integer()
-    urgente = fields.Boolean(compute="_value_urgente", store=True)
+    urgente = fields.Boolean(compute="_value_pc", store=True)
     realizada = fields.Boolean()
-
+    descripcion = fields.Text()
 
     #Este computo depende de la variable prioridad
     @api.depends('prioridad')
     #Funcion para calcular el valor de urgente
-    def _value_urgente(self):
+    def _value_pc(self):
         #Para cada registro
         for record in self:
-            #Si la prioridad es mayor que 10, se considera urgente, en otro caso no lo es
-            if record.prioridad>10:
+            #Si la prioridad es mayor que 8, se considera urgente, en otro caso no lo es
+            if record.prioridad>8:
                 record.urgente = True
             else:
                 record.urgente = False
