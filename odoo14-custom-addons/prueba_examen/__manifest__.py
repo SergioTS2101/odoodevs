a7f2ac32045c6e1f7f7813fb986e69c76a22b4b1
SergioTS2101 <sergiots2101@gmail.com>
2022-03-01 20:41:46 +0100

    Creando nuevo módulo

--- odoo14-custom-addons/prueba_examen/__manifest__.py
+++ odoo14-custom-addons/prueba_examen/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "pruebaExamen",
+    'name': "prueba_examen",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,13 +10,14 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Sergio Tomeo Samuel",
+    'website': "https://github.com/SergioTS2101/odoodevs/tree/beyondlists",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'application': True,
+    'category': 'Marketing',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,7 +25,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
--- odoo14-custom-addons/prueba_examen/models/models.py
+++ odoo14-custom-addons/prueba_examen/models/models.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class prueba_examen(models.Model):
-#     _name = 'prueba_examen.prueba_examen'
-#     _description = 'prueba_examen.prueba_examen'
+class prueba_examen(models.Model):
+    _name = 'prueba_examen.prueba_examen'
+    _description = 'prueba_examen.prueba_examen'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    pepe = fields.Text()
+    sergio = fields.Text()
+    guarni = fields.Text()
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
